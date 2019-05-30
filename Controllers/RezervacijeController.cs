a32ec35a5b670d53e641020c4f66aef6b9c5773d
Faruk Ohran <faruk.ohran@edu.fit.ba>
2019-05-30 12:15:34 +0200

    komit

---
+++ Controllers/RezervacijeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TurAgencijaRS2_Model.Requests;
+using TurAgencijaRS2_WebApi.Services;
+
+namespace TurAgencijaRS2_WebApi.Controllers
+{
+   
+    public class RezervacijeController : BaseCRUDController<TurAgencijaRS2_Model.Rezervacije, object, RezervacijeUpsertRequest, RezervacijeUpsertRequest>
+    {
+        public RezervacijeController(ICRUDService<TurAgencijaRS2_Model.Rezervacije, object, RezervacijeUpsertRequest, RezervacijeUpsertRequest> service) : base(service)
+        {
+
+        }
+
+    }
+}
